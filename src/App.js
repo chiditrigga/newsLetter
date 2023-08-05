70edabed2353dda8cbb49a2427e2480d0dd1bb84
daniel <chididaniel1998@gmail.com>
2023-08-04 17:04:00 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './images/style.css';
 import Home from './home';
 
-import {BrowserRouter, Routes, Route,Link} from 'react-router-dom'
+
 import Success from './success';
 
 
@@ -10,15 +10,8 @@ function App({email}) {
   return (
 
     <div className="App">
-      <BrowserRouter>
-       <Routes>
-         <Route path='/' element={ <Home email />} />
-         <Route path='success' element={<Success emails={email}/>} />
-       </Routes>
-      
-      </BrowserRouter>
-      
-      
+      <Home />
+        
       
       
       
